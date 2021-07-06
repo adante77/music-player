7097f4078198de41c897da674e5898579a0ca403
adante77 <76264477+adante77@users.noreply.github.com>
2021-07-06 17:04:05 +0430

    update

---
+++ pages/fallback.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+
+export default () =>(
+    <>
+        <Head>
+            <title> next-pwa example</title>
+        </Head>
+        <div>
+            this is offline
+        </div>
+    </>
+)
